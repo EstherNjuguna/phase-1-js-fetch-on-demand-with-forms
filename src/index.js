592b3a458db906360eaf8e585084c56953b0d0c1
+ <+>
2022-06-15 22:29:31 +0300

    commit tree 4015a9613ee681169f39004af85db314a03f0078

--- src/index.js
+++ src/index.js
@@ -1,16 +1,12 @@
-const init = () => {
-    const inputForm = document.querySelector('form');
-  
-    inputForm.addEventListener('submit', (event) => {
-        event.preventDefault();
-        const input = document.querySelector('input#searchByID');
-      
-        fetch(`http://localhost:3000/movies/${input.value}`)
-        .then(response => response.json())
-        .then(data => {
-          console.log(data);
-        });
-      });
-    }
-  
-  document.addEventListener('DOMContentLoaded', init);
+const configurationObject = {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+    Accept: "application/json",
+  },
+  body: JSON.stringify({
+    name:"",email:""
+  }),
+};
+
+fetch("http://localhost:3000/users", configurationObject);
