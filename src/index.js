70b48ed2c260119411ce96031d4feeb2829f41e7
+ <+>
2022-06-15 08:32:19 +0300

    final

--- src/index.js
+++ src/index.js
@@ -8,12 +8,9 @@ const init = () => {
         fetch(`http://localhost:3000/movies/${input.value}`)
         .then(response => response.json())
         .then(data => {
-          const title = document.querySelector('section#movieDetails h4');
-          const summary = document.querySelector('section#movieDetails p');
-          title.innerText = data.title;
-    summary.innerText = data.summary;
+          console.log(data);
         });
-        });
-      
+      });
     }
-      document.addEventListener('DOMContentLoaded', init); 
+  
+  document.addEventListener('DOMContentLoaded', init);
